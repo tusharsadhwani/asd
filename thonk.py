e4241e5e36200ade66a2f654a3930e4e10150007
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2020-12-08 18:10:39 +0530

    Update thonk

--- thonk.py
+++ thonk.py
@@ -1,26 +1,27 @@
-import os
+import sys
 from time import sleep
+from typing import Generator
 
 
-def clear():
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        os.system('clear')
-
-
-def dots(text):
-    yield text + '.'
-    yield text + '..'
-    yield text + '...'
-
-
-def main():
+def get_dots() -> Generator[str, None, None]:
     while True:
-        for text in dots('Please wait\nDTL is thonking'):
-            clear()
-            print(text, end='')
-            sleep(.6)
+        yield '.'
+        yield '..'
+        yield '...'
+
+
+def main() -> None:
+    print('Please wait, DTL is thonking', end='')
+    for dots in get_dots():
+        sys.stdout.write(dots)
+        sys.stdout.flush()
+
+        sleep(.6)
+
+        dot_count = len(dots)
+        sys.stdout.write('\b' * dot_count)
+        sys.stdout.write(' ' * dot_count)
+        sys.stdout.write('\b' * dot_count)
 
 
 if __name__ == "__main__":
