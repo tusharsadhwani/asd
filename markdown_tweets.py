9276c92c2446ad5610958212f85ee873af35dd73
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2022-01-26 19:43:45 +0530

    Rename to api key

--- markdown_tweets.py
+++ markdown_tweets.py
@@ -12,8 +12,10 @@ import tweepy
 from tweepy import Media
 from tweepy.errors import TweepyException
 
-consumer_key = ""
-consumer_secret = ""
+
+# Add your twitter info here:
+api_key = ""
+api_secret = ""
 access_token = ""
 access_token_secret = ""
 
@@ -59,7 +61,7 @@ def tweet_length(tweet: str) -> int:
 
 
 def twitter_login() -> tweepy.API:
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth = tweepy.OAuthHandler(api_key, api_secret)
     auth.set_access_token(access_token, access_token_secret)
 
     api = tweepy.API(auth)
